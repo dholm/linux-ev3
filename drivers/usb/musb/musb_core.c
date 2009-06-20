4f23bccd93964457e1675ee1311e11d57341fcdf
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-06-20 14:06:38 +0530

    musb: Add back old musb procfs file
    Procfs file is removed from musb and an equivalent debugfs is
    expected to replace it.
    
    Adding the old file back to retain the debug functionalities untill
    we get debugfs equivalent in mainline.
    
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>
    Signed-off-by: Ravi B <a0393587@psplinux051.india.ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -2036,8 +2036,6 @@ bad_config:
 		musb->xceiv->state = OTG_STATE_A_IDLE;
 
 		status = usb_add_hcd(musb_to_hcd(musb), -1, 0);
-		if (status)
-			goto fail;
 
 		DBG(1, "%s mode, status %d, devctl %02x %c\n",
 			"HOST", status,
@@ -2052,8 +2050,6 @@ bad_config:
 		musb->xceiv->state = OTG_STATE_B_IDLE;
 
 		status = musb_gadget_setup(musb);
-		if (status)
-			goto fail;
 
 		DBG(1, "%s mode, status %d, dev%02x\n",
 			is_otg_enabled(musb) ? "OTG" : "PERIPHERAL",
@@ -2067,7 +2063,8 @@ bad_config:
 #endif
 	if (status)
 		goto fail2;
-
+	if (status == 0)
+		musb_debug_create("driver/musb_hdrc", musb);
 	return 0;
 
 fail2:
@@ -2130,6 +2127,7 @@ static int __exit musb_remove(struct platform_device *pdev)
 	 *  - OTG mode: both roles are deactivated (or never-activated)
 	 */
 	musb_shutdown(pdev);
+	musb_debug_delete("driver/musb_hdrc", musb);
 #ifdef CONFIG_USB_MUSB_HDRC_HCD
 	if (musb->board_mode == MUSB_HOST)
 		usb_remove_hcd(musb_to_hcd(musb));
