fa80967c739c79f044782bf42db7bea0e72edd18
Melissa Watkins <m-watkins@ti.com>
2010-11-12 09:59:14 -0600

    pru_uart_can_include
    Patches include files of the 2.6.33-rc4 kernel to support
    UART and CAN emulation by the PRU.

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -182,6 +182,9 @@
 /* Aeroflex Gaisler GRLIB APBUART */
 #define PORT_APBUART    90
 
+/* omapl pru uart emulation */
+#define OMAPL_PRU_SUART	91
+
 #ifdef __KERNEL__
 
 #include <linux/compiler.h>
